691e774c2c2db41683d34c6954e9d76623a806f9
matt <r4nd3l8888@gmail.com>
2022-03-22 17:48:26 +0100

    angular2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    p{
+      color: blue;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'my-first-app';
 }
